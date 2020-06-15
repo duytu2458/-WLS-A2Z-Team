aa94a9e525ea46f1ff8d070bc87556cfa8d66ec8
duytu2458 <duytu2458@gmail.com>
2020-06-15 16:51:47 +0700

    update demo4

--- CM_Plan/Users/TuND/demo4/src/main/java/vn/vtd/wls/DemoApplication.java
+++ CM_Plan/Users/TuND/demo4/src/main/java/vn/vtd/wls/DemoApplication.java
@@ -4,16 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan({ "vn.vtd.wls.admin.controller", "vn.vtd.wls.admin.service", "vn.vtd.wls.demo", "vn.vtd.wls.model",
-//		"vn.vtd.wls.admin.repositories" })
 
-//@ComponentScans(value = { @ComponentScan("vn.vtd.wls.admin.controller"),
-//        @ComponentScan("vn.vtd.wls.admin.service"),
-//        @ComponentScan("vn.vtd.wls.admin.repositories"),
-//        @ComponentScan("vn.vtd.wls.model"),
-//        @ComponentScan("vn.vtd.wls.demo")
-//})
 @SpringBootApplication
+@ComponentScan({"vn.vtd.wls"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
